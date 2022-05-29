4c53b0fc957fadc9eb82b5fe5e232ff8e706b146
Andreas Lehmann <alehmann42@gmail.com>
2022-05-29 20:08:00 +0200

    Heelo World

--- TravelNG/urls.py
+++ TravelNG/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from hello.views import hello_world
+
 urlpatterns = [
+    path('hello/', hello_world),
     path('admin/', admin.site.urls),
 ]
