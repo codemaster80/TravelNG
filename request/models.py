eca1be4119489f6a77056c4becb780d32662f057
Andreas Lehmann <alehmann42@gmail.com>
2022-06-02 22:58:04 +0200

    database model

--- request/models.py
+++ request/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
+
 # Create your models here.
+class Employee(models.Model):
+    name = models.CharField(max_length=30)
+    surname = models.CharField(max_length=30)
+    email = models.CharField(max_length=50)
+
+
+class CostCenter(models.Model):
+    department = models.CharField(max_length=50)
+    cost_center_number = models.IntegerField()
+
+
+class TravelRequest(models.Model):
+    destination = models.CharField(max_length=50)
+    event = models.CharField(max_length=50)
+    cost_center = models.ForeignKey(CostCenter, on_delete=models.SET_NULL, null=True)
+    employee = models.ForeignKey(Employee, on_delete=models.SET_NULL, null=True)
