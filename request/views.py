e2dd1668209f2eceb47314d47c2660df7dc1af44
Andreas Lehmann <alehmann42@gmail.com>
2022-08-13 18:38:59 +0200

    views improved #2

--- request/views.py
+++ request/views.py
@@ -12,9 +12,10 @@ from request.forms import *
 @login_required
 def home(request):
     travel_requests = TravelRequest.objects.all().order_by('journey_start')
+    travel_invoices = TravelInvoice.objects.all()
 
     return render(request, 'request/travelRequestList.html',
-                  {'page_title': '', 'travel_requests': travel_requests})
+                  {'page_title': '', 'travel_requests': travel_requests, 'travel_invoices': travel_invoices})
 
 
 @login_required
@@ -51,24 +52,36 @@ def delete_travel_request(request, item_id):
 
 
 @login_required
-def new_travel_invoice(request):
-    travel_invoice = TravelInvoice()
+def delete_travel_invoice(request, item_id):
+    travel_invoice = TravelInvoice.objects.get(id=item_id)
+    travel_invoice.delete()
+    return HttpResponseRedirect(reverse('home'))
+
+
+@login_required
+def travel_invoice_details(request, pk=None):
+    if pk is None:
+        travel_invoice = TravelInvoice()
+        page_title = 'Reisekostenabrechnung'
+        sub_title = 'Antrag erstellen'
+    else:
+        travel_invoice = get_object_or_404(TravelInvoice, pk=pk)
+        page_title = 'Reisekostenabrechnung'
+        sub_title = 'Antrag ändern'
 
     if request.method == 'POST':
-        # Formular abgeschickt
-        form = InvoiceForm(request.POST, request.FILES, instance=travel_invoice)
+        form = InvoiceForm(request.POST, instance=travel_invoice)
         if form.is_valid():
             form.save()
-            # Pruefung erfolgreich
+            messages.success(request, 'Gespeichert')
             return HttpResponseRedirect(reverse('home'))
         else:
-            # Pruefung nicht erfolgreich
-            pass
+            messages.error(request, 'Bitte Fehler korrigieren')
     else:
         form = InvoiceForm(instance=travel_invoice)
 
     return render(request, 'request/travelForm.html',
-                  {'page_title': 'Reisekostenabrechnung', 'sub_title': 'Antrag erstellen', 'form': form})
+                  {'page_title': page_title, 'sub_title': sub_title, 'form': form})
 
 
 @login_required
