116c6134740ce84ed5e09a42197582d0532a1e82
Andreas Lehmann <alehmann42@gmail.com>
2022-09-13 00:13:57 +0200

    static root added

--- TravelNG/settings.py
+++ TravelNG/settings.py
@@ -113,6 +113,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = 'static/'
 
 # Uploaded files (PDF, Images)
 MEDIA_ROOT = 'travel/upload/'
